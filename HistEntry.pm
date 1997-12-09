715cb66beba966f6c20c056c2fde2a4b24915d5a
eserte <eserte>
1997-12-09 02:34:02 +0000

    Optionen -command und -bell historycount zu historyindex umbenannt

--- HistEntry.pm
+++ HistEntry.pm
@@ -1,7 +1,7 @@
 # -*- perl -*-
 
 #
-# $Id: HistEntry.pm,v 1.2 1997/12/06 17:31:03 eserte Exp $
+# $Id: HistEntry.pm,v 1.3 1997/12/09 02:34:02 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright © 1997 Slaven Rezic. All rights reserved.
@@ -16,17 +16,21 @@
 
 package Tk::HistEntry;
 require Tk::BrowseEntry;
-@ISA = qw(Tk::BrowseEntry);
+@ISA = qw(Tk::Derived Tk::BrowseEntry);
 Construct Tk::Widget 'HistEntry';
 
-$VERSION = '0.10';
+$VERSION = '0.11';
 
 sub Populate {
     my($w, $args) = @_;
     $args->{'-variable'} = delete $args->{'-textvariable'} 
         if defined $args->{'-textvariable'};
-#    $args->{'-browsecmd'} = sub { $w->historySet($_[0]) };
+    $w->{Configure}{-command} = delete $args->{'-command'};
+    $w->{Configure}{-bell}    =
+      (!exists $args->{'-bell'} ? 1 : delete $args->{'-bell'});
+#    $args->{'-browsecmd'} = sub { $w->historySet($_[1]) };
     $w->SUPER::Populate($args);
+#    $w->ConfigSpecs(-autoadd => ['PASSIVE', 'autoadd', 'Autoadd', 0]);
 }
 
 sub SetBindtags {
@@ -41,6 +45,13 @@ sub addBind {
     $w->bind('<Down>'      => sub { $w->historyDown });
     $w->bind('<Control-r>' => sub { $w->searchBack });
     $w->bind('<Control-s>' => sub { $w->searchForw });
+    if ($w->{Configure}{-command}) {
+	$w->bind('<Return>' => sub {
+		     return unless defined $ {$w->cget(-textvariable)};
+		     $w->historyAdd($ {$w->cget(-textvariable)});
+		     &{$w->{Configure}{-command}}($w);
+		 });
+    }
 }
 
 sub historyAdd {
@@ -48,28 +59,28 @@ sub historyAdd {
     $line = ${$w->cget(-textvariable)} if !defined $line;
     if ($line ne $w->{'history'}->[$#{$w->{'history'}}]) {
 	push(@{$w->{'history'}}, $line);
-	$w->{'historycount'} = $#{$w->{'history'}};
+	$w->{'historyindex'} = $#{$w->{'history'}} + 1;
 	$w->insert('end', $line);
     }
 }
 
 sub historyUp {
     my $w = shift;
-    if ($w->{'historycount'} > 0) {
-	$w->{'historycount'}--;
-	${$w->cget(-textvariable)} = $w->{'history'}->[$w->{'historycount'}];
+    if ($w->{'historyindex'} > 0) {
+	$w->{'historyindex'}--;
+	${$w->cget(-textvariable)} = $w->{'history'}->[$w->{'historyindex'}];
     } else {
-	$w->toplevel->bell;
+	$w->_bell;
     }
 }
 
 sub historyDown {
     my $w = shift;
-    if ($w->{'historycount'} <= $#{$w->{'history'}}) {
-	$w->{'historycount'}++;
-	${$w->cget(-textvariable)} = $w->{'history'}->[$w->{'historycount'}];
+    if ($w->{'historyindex'} <= $#{$w->{'history'}}) {
+	$w->{'historyindex'}++;
+	${$w->cget(-textvariable)} = $w->{'history'}->[$w->{'historyindex'}];
     } else {
-	$w->toplevel->bell;
+	$w->_bell;
     }
 }
 
@@ -78,7 +89,7 @@ sub historySet {
     my $i;
     for($i = $#{$w->{'history'}}; $i >= 0; $i--) {
 	if ($index eq $w->{'history'}->[$i]) {
-	    $w->{'historycount'} = $i;
+	    $w->{'historyindex'} = $i;
 	    last;
 	}
     }
@@ -87,34 +98,40 @@ sub historySet {
 # XXX search ist nicht so intuitiv wie bei gnu-readline
 sub searchBack {
     my $w = shift;
-    my $i = $w->{'historycount'}-1;
+    my $i = $w->{'historyindex'}-1;
     while ($i >= 0) {
-	my $search = ${$w->cget(-textvariable)};
+	my $search = $ {$w->cget(-textvariable)};
         if ($search eq substr($w->{'history'}->[$i], 0, length($search))) {
-	    $w->{'historycount'} = $i;
-	    ${$w->cget(-textvariable)} 
-                = $w->{'history'}->[$w->{'historycount'}];
+	    $w->{'historyindex'} = $i;
+	    $ {$w->cget(-textvariable)} 
+                = $w->{'history'}->[$w->{'historyindex'}];
             return;
         }
         $i--;
     }
-    $w->toplevel->bell;
+    $w->_bell;
 }
 
 sub searchForw {
     my $w = shift;
-    my $i = $w->{'historycount'}+1;
+    my $i = $w->{'historyindex'}+1;
     while ($i <= $#{$w->{'history'}}) {
-	my $search = ${$w->cget(-textvariable)};
+	my $search = $ {$w->cget(-textvariable)};
         if ($search eq substr($w->{'history'}->[$i], 0, length($search))) {
-	    $w->{'historycount'} = $i;
-	    ${$w->cget(-textvariable)} 
-                = $w->{'history'}->[$w->{'historycount'}];
+	    $w->{'historyindex'} = $i;
+	    $ {$w->cget(-textvariable)} 
+                = $w->{'history'}->[$w->{'historyindex'}];
             return;
         }
         $i++;
     }
-    $w->toplevel->bell;
+    $w->_bell;
+}
+
+sub _bell {
+    my $w = shift;
+    return unless $w->{Configure}{-bell};
+    $w->bell;
 }
 
 1;
