11809ab76fff4a2e802d5d60f5515d9c51369eeb
eserte <eserte>
1997-12-12 23:21:04 +0000

    new button: replace history

--- examples/1.pl
+++ examples/1.pl
@@ -1,3 +1,4 @@
+# -*- perl -*-
 # Before `make install' is performed this script should be runnable with
 # `make test'. After `make install' it should work as `perl test.pl'
 
@@ -7,7 +8,8 @@ BEGIN { $^W = 1; $| = 1; $loaded = 0; print "1..2\n"; }
 END {print "not ok 1\n" unless $loaded;}
 use Tk::HistEntry;
 use strict;
-my $loaded = 1;
+use vars qw($loaded);
+$loaded = 1;
 print "ok 1\n";
 
 ######################### End of black magic.
@@ -33,6 +35,11 @@ my $bb = $f->Button(-text => 'Add current',
 			$lb->see('end');
 			$foo = '';
 		    })->pack;
+$f->Button(-text => 'Replace history',
+	   -command => sub {
+	       $b->history([keys %ENV]);
+	   }
+	  )->pack;
 $b->bind('<Return>' => sub { $bb->invoke });
 
 my $f2 = $top->Frame->grid(-row => 1, -column => 0, -sticky => 'n');
@@ -73,8 +80,6 @@ my $b3 = $f3->SimpleHistEntry(-textvariable => \$blubber,
 			      })->pack;
 $f3->Button(-text => 'Add current',
 	    -command => sub { $b3->invoke })->pack;
-
-package main;
 
 # Autodestroy
 my $seconds = 60;
